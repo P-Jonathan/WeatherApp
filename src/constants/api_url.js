036b40362185e0efa0d4ede20e591e0ff612baeb
John <54717259+P-Jonathan@users.noreply.github.com>
2020-01-10 13:32:03 -0300

    Update api_url.js

--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,4 +1,4 @@
-export const api_key = pocess.env.WEATHER_API_KEY;
+export const api_key = process.env.WEATHER_API_KEY;
 export const action_default = 'weather';
 export const action_forecast = 'forecast';
 export const url_base_weather = "http://api.openweathermap.org/data/2.5/";
